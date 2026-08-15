2c49cb24f3e51e2caf63c04bec53af22b84d98e4
Your Name <your_email@example.com>
2026-08-15 16:46:11 +0800

    Add Nix flake build support

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "A Wayland status bar for mangowm";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          mangobar = pkgs.stdenv.mkDerivation {
+            pname = "mangobar";
+            version = "0.1.1";
+            src = self;
+
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+              python3
+              wayland-scanner
+              wayland-protocols
+            ];
+
+            buildInputs = with pkgs; [
+              alsa-lib
+              cairo
+              cjson
+              fcft
+              gdk-pixbuf
+              libpulseaudio
+              pango
+              pixman
+              systemd
+              wayland
+            ];
+
+            meta = with pkgs.lib; {
+              description = "A Wayland status bar for mangowm";
+              homepage = "https://github.com/mangowm/mangobar";
+              license = licenses.mit;
+              platforms = platforms.linux;
+              mainProgram = "mangobar";
+            };
+          };
+        in
+        {
+          inherit mangobar;
+          default = mangobar;
+        });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.mangobar ];
+          };
+        });
+    };
+}
